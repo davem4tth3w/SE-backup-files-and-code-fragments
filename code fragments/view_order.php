434740293d42f858a470609f654514cd34c76149
Dave Matthew <143525765+davem4tth3w@users.noreply.github.com>
2024-03-05 11:35:47 +0800

    Update view_order.php

--- code fragments/view_order.php
+++ code fragments/view_order.php
@@ -35,7 +35,7 @@ if(isset($_GET["pdf"]) && isset($_GET['order_id']))
 				<table width="100%" cellpadding="5">
 					<tr>
 						<td width="65%">
-							To,<br />
+							
 							<b>RECEIVER (BILL TO)</b><br />
 							Name : '.$row["inventory_order_name"].'<br />	
 							Billing Address : '.$row["inventory_order_address"].'<br />
@@ -50,17 +50,16 @@ if(isset($_GET["pdf"]) && isset($_GET['order_id']))
 				<br />
 				<table width="100%" border="1" cellpadding="5" cellspacing="0">
 					<tr>
-						<th rowspan="2">Sr No.</th>
+						<th rowspan="2">No.</th>
 						<th rowspan="2">Product</th>
 						<th rowspan="2">Quantity</th>
 						<th rowspan="2">Price</th>
 						<th rowspan="2">Actual Amt.</th>
-						<th colspan="2">VAT (%12)</th>
-						<th rowspan="2">Total</th>
+		
+						
 					</tr>
 					<tr>
-						<th>Rate</th>
-						<th>Amt.</th>
+					
 					</tr>
 		';
 		$statement = $connect->prepare("
@@ -76,21 +75,19 @@ if(isset($_GET["pdf"]) && isset($_GET['order_id']))
 		$count = 0;
 		$total = 0;
 		$total_actual_amount = 0;
-		$total_tax_amount = 0;
+		$overall_total = 0;
 		$vat_percentage = 0;
 		$discount = 0;
-
-
 
 		foreach($product_result as $sub_row)
 		{
 			$count = $count + 1;
 			$product_data = fetch_product_details($sub_row['product_id'], $connect);
 			$actual_amount = $sub_row["quantity"] * $sub_row["price"];
-			$tax_amount = ($actual_amount * $sub_row["tax"])/100;
-			$total_product_amount = $actual_amount + $tax_amount;
+
+			$total_product_amount = $actual_amount;
 			$total_actual_amount = $total_actual_amount + $actual_amount;
-			$total_tax_amount = $total_tax_amount + $tax_amount;
+
 			$total = $total + $total_product_amount;
 
 
@@ -99,12 +96,9 @@ $vat_percentage = $row['vat_percentage']; // Fetch VAT percentage from the order
 $discount = $row['discount']; // Fetch discount from the order
 $total = $row['inventory_order_total']; // Fetch total amount from the order
 
-$vat = ($total * $vat_percentage) / 100;
-$overall_total = $total + $vat - $discount;
-
-
-
-
+// $vat = ($total * $vat_percentage) / 100;
+$vat_percetage = ($total * $vat_percentage) / 100;
+$overall_total = $total;
 
 // Update the inventory_order table with the calculated values
 $update_statement = $connect->prepare("
@@ -125,12 +119,13 @@ $update_statement->execute(array(
 				<tr>
 					<td>'.$count.'</td>
 					<td>'.$product_data['product_name'].'</td>
+					
 					<td>'.$sub_row["quantity"].'</td>
 					<td aling="right">'.$sub_row["price"].'</td>
 					<td align="right">'.number_format($actual_amount, 2).'</td>
-					<td>'.$sub_row["tax"].'%</td>
-					<td align="right">'.number_format($tax_amount, 2).'</td>
-					<td align="right">'.number_format($total_product_amount, 2).'</td>
+					
+					
+			
 				</tr>
 			';
 		}
@@ -138,14 +133,14 @@ $update_statement->execute(array(
 		<tr>
 			<td align="right" colspan="4"><b>Total</b></td>
 			<td align="right"><b>'.number_format($total_actual_amount, 2).'</b></td>
-			<td>&nbsp;</td>
-			<td align="right"><b>'.number_format($total_tax_amount, 2).'</b></td>
-			<td align="right"><b>'.number_format($total, 2).'</b></td>
+			
+		
+			
 		</tr>
 
 
 		<tr>
-    <td colspan="4" align="right"><b>VAT (12%)</b></td>
+    <td colspan="4" align="right"><b>VAT</b></td>
     <td colspan="3" align="right">'.number_format($vat_percentage, 2).'</td>
 </tr>
 <tr>
@@ -153,7 +148,7 @@ $update_statement->execute(array(
     <td colspan="3" align="right">'.number_format($discount, 2).'</td>
 </tr>
 <tr>
-    <td colspan="4" align="right"><b>Overall Ttal</b></td>
+    <td colspan="4" align="right"><b>Overall Total:</b></td>
     <td colspan="3" align="right">'.number_format($overall_total, 2).'</td>
 </tr>
 
